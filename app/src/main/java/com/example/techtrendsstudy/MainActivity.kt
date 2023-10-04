b9c6be8eeb805e2cb6322e29248b1778c76a41eb
rlaxodud214 <rlaxodud214@naver.com>
2023-10-04 23:06:47 +0900

    Jetpack Compose 4 - List UI -> LazyColumn

--- app/src/main/java/com/example/techtrendsstudy/MainActivity.kt
+++ app/src/main/java/com/example/techtrendsstudy/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -31,32 +35,62 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // setContent 안에서만 Compose 객체를 사용할 수 있다.
         setContent {
+            val scrollState = rememberScrollState()
             TechTrendsStudyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    /* 모든 Composable 객체들은 기본적으로 modifier를 사용할 수 있다.
-                       Box는 자식들이 다 겹치는 속성을 가지고 있어 FrameLayout과 유사하다.
-                       Box(
+                    /* Jetpack Compose 4 - List(기존 ListView), LazyColumn(기존 RecyclerView)
+                       데이터의 개수가 많을 때, 많은 리소스가 발생하므로 LazyColumn를 사용한다.
+
+                       LazyColumn(
                            modifier: Modifier = Modifier,
-                           contentAlignment: Alignment = Alignment.TopStart,
-                           propagateMinConstraints: Boolean = false,
-                           content: @Composable BoxScope.() -> Unit
-                       Column과 Row, Box를 사용하여 큰 틀의 레이아웃을 구성하면 된다. -> RN, 플러터와 같음
-                     */
-                    Box(modifier = Modifier
-                        .background(color = Color.Gray)
-                        .fillMaxWidth()
-                        .height(100.dp)
-                        .padding(16.dp),
-                        contentAlignment = Alignment.TopStart
+                           state: LazyListState = rememberLazyListState(),
+                           contentPadding: PaddingValues = PaddingValues(0.dp),
+                           reverseLayout: Boolean = false,
+                           verticalArrangement: Arrangement.Vertical =
+                               if (!reverseLayout) Arrangement.Top else Arrangement.Bottom,
+                           horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+                           flingBehavior: FlingBehavior = ScrollableDefaults.flingBehavior(),
+                           userScrollEnabled: Boolean = true,
+                           content: LazyListScope.() -> Unit
+                    */
+                    LazyColumn(
+                        modifier = Modifier
+                            .background(color = Color.Green)
+                            .fillMaxWidth(),
+                        contentPadding = PaddingValues(16.dp), // 외부 객체 와의 패딩
+                        verticalArrangement = Arrangement.spacedBy(4.dp), // 내부 객체 간의 패딩
                     ) {
-                        Text("TopStart")
-                        Box(modifier = Modifier.fillMaxSize(),
-                            contentAlignment = Alignment.BottomEnd) {
-                            Text("BottomEnd")
+                        item {
+                            Text("Header")
+                        }
+                        /*
+                        items(
+                            count: Int,
+                            key: ((index: Int) -> Any)? = null,
+                            contentType: (index: Int) -> Any? = { null },
+                            itemContent: @Composable LazyItemScope.(index: Int) -> Unit
+
+                         */
+                        items(100) { index ->
+                            Text("List 유사 객체[$index]")
+                        }
+                        item {
+                            Text("Footer")
                         }
                     }
+                    // 아래의 Column을 위와 같이 LazyColumn으로 변경
+//                    Column(modifier = Modifier
+//                        .background(color = Color.Green)
+//                        .fillMaxWidth()
+//                        .verticalScroll(scrollState)
+//                    ) {
+//                        for (i in 1..50) {
+//                            Text("$i")
+//                        }
+//                    }
                 }
             }
         }
