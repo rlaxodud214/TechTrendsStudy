68dcc0461d3b5f09bc0ec1bc59e6e39442cbc74d
rlaxodud214 <rlaxodud214@naver.com>
2023-10-08 00:01:03 +0900

    JC5 - Image, Card, State / 커스텀 composable 객체 생성, 상태 및 상태변화 외부에서 관리하는 법

--- app/src/main/java/com/example/techtrendsstudy/MainActivity.kt
+++ app/src/main/java/com/example/techtrendsstudy/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.techtrendsstudy
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -20,14 +21,26 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -45,22 +58,20 @@ class MainActivity : ComponentActivity() {
 
         // setContent 안에서만 Compose 객체를 사용할 수 있다.
         setContent {
+            // 상태값 저장에선 remember보단 rememberSaveable를 기본적으로 많이 쓰게 될 듯
+            // rememberSaveable : 화면 회전 및 환경구성 변경에도 상태 보존(임시적인 상태 저장으로 내장 DB를 대체할 수는 없음)
+            // isFavorite의 자료형 : MutableState<Boolean>
+            var isFavorite by rememberSaveable{
+                mutableStateOf(false)
+            }
             TechTrendsStudyTheme {
-                LazyColumn(
+                ImageCard(
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    contentPadding = PaddingValues(16.dp), // 외부 객체 와의 패딩
-                    verticalArrangement = Arrangement.spacedBy(4.dp), // 내부 객체 간의 패딩
+                        .fillMaxWidth(0.5f)
+                        .padding(16.dp),
+                    isFavorite = isFavorite,
                 ) {
-                    item {
-                        ImageCard(ContentScale.Crop) // 아래-위 잘림
-                    }
-                    item {
-                        ImageCard(ContentScale.Fit) // 원본 이미지 비율을 객체 크기에 맞게 수정
-                    }
-                    item {
-                        ImageCard(ContentScale.FillBounds) // Fit에서 여백 없게 확장
-                    }
+                    favorite -> isFavorite = favorite
                 }
             }
         }
@@ -69,21 +80,14 @@ class MainActivity : ComponentActivity() {
 
 // Jetpack Compose 5 - Image, Card, State
 @Composable
-fun ImageCard(cs : ContentScale) {
-    /* 기존 CardView
+fun ImageCard( // 커스텀 UI 만들기 - RN과 똑같음
+    modifier: Modifier = Modifier, // Default Value 지정
+    isFavorite: Boolean, // 변수x -> 상수
+    onTabFavorite: (Boolean) -> Unit,
+) {
+    // 기존 CardView
     Card(
-        modifier: Modifier = Modifier,
-        shape: Shape = CardDefaults.shape,                       // 외각라인 둥글게
-        colors: CardColors = CardDefaults.cardColors(),
-        elevation: CardElevation = CardDefaults.cardElevation(), // 그림자
-        border: BorderStroke? = null,
-        content: @Composable ColumnScope.() -> Unit
-   */
-
-    Card(
-        modifier = Modifier
-            .fillMaxWidth(0.5f)
-            .padding(16.dp),
+        modifier = modifier,
         shape = RoundedCornerShape(8.dp), // 카드 외각라인 둥글게하는 부분
         elevation = CardDefaults.cardElevation(5.dp), // 외각라인 그림자 반영
     ) {
@@ -91,21 +95,27 @@ fun ImageCard(cs : ContentScale) {
             modifier = Modifier.height(200.dp)
         ) {
             // 로컬의 이미지 파일을 출력할 때는 Painter를 매개 값으로 사용한다.
-            /* 기존의 ImageView와 같음
-            Image(
-                painter: Painter,
-                contentDescription: String?,
-                modifier: Modifier = Modifier,
-                alignment: Alignment = Alignment.Center,
-                contentScale: ContentScale = ContentScale.Fit,
-                alpha: Float = DefaultAlpha,
-                colorFilter: ColorFilter? = null
-             */
+            // 기존 ImageView
             Image(
                 painter = painterResource(id = R.drawable.poster),
-                contentDescription = "poster", // content 설명 텍스트 필수로 넣어야 함.
-                contentScale = cs, // https://developer.android.com/jetpack/compose/graphics/images/customize?hl=ko
+                contentDescription = null, // content 설명 텍스트 필수로 넣어야 함 -> null 허용
+                // ContentScale의 속성 ref : https://developer.android.com/jetpack/compose/graphics/images/customize?hl=ko
+                contentScale = ContentScale.Crop,
             )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd,
+            ) {
+                IconButton(onClick = {
+                    onTabFavorite.invoke(!isFavorite)
+                }) {
+                    Icon(
+                        imageVector = if(isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = null,
+                        tint = Color.White,
+                    )
+                }
+            }
         }
     }
 }
